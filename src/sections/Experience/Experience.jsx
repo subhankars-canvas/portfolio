82c9608e0ced0d655021fd73b71073e44056fcdb
painter on canvas <roysubho687@gmail.com>
2022-06-21 15:08:30 -0700

    experience and skills section

--- src/sections/Experience/Experience.jsx
+++ src/sections/Experience/Experience.jsx
@@ -1,6 +1,106 @@
 import React from 'react'
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import { getJobDetails, getTechs } from '../../utils/utils'
+import { styled } from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
+
+
+const ColorButton = styled(Button)(({ theme }) => ({
+    color: '#fff',
+    backgroundColor: '#ed4747',
+    '&:hover': {
+      backgroundColor: 'red',
+    },
+  }));
 function Experience() {
-    return <h1>hi exp</h1>
+    return (
+        <>
+            <Grid container spacing={2}>
+                <Grid item xs="12">
+                    <Typography 
+                        variant='h5'
+                        sx={{
+                            mb: 4,
+                            fontFamily: 'Oxygen',
+                            fontWeight: '300',
+                            textAlign: 'center',
+                        }}
+                    >
+                        A full stack developer experieced in Javascript frameworks as well as in backend techs like PHP, Laravel, MySql. Self made journey begins with Tier5 Technology Solutions Pvt. Ltd and right now at Larsen & Toubro group of Company.
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Grid container spacing={1} className="p-4">
+                {
+                    getJobDetails().map((el,i, arr) => {
+                        return (
+                        <Grid item xs={4}>
+                            <Card className="border-2 border-[#bdbfc3]">
+                                <CardMedia
+                                    component="img"
+                                    height="140"
+                                    image={el.image}
+                                    alt={el.imgAlt}
+                                    className={'border-b-2'}
+                                >
+                                </CardMedia>
+                                <CardContent className='relative'>
+                                    {
+                                        el.isCurrent && <div className='absolute -right-7 -top-16 bg-secondary text-[#fff] p-2 pt-3 rotate-90 rounded uppercase font-thin text-sm animate-pulse'>Current</div>
+                                    }
+                                    <div className='flex justify-between'>
+                                        <div className='text-xl my-2 font-medium font-oxygen text-primary'>{el.companyName}</div>
+                                        <div className='bg-no-repeat bg-center h-14 w-14'>
+                                            <img src={el.companyLogo} alt="logo"/>
+                                        </div>
+                                    </div>
+                                    <Typography variant="body2" color="text.secondary" className='font-inherit'>{el.role}</Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <ColorButton variant="contained">Learn More</ColorButton>
+                                </CardActions>
+                            </Card>
+                        </Grid>)
+                    })
+                }
+            </Grid>
+            <Grid container spacing={2} className="p-4">
+                <Grid item xs="12">
+                    <Typography 
+                        variant='h6'
+                        sx={{
+                            mb: 4,
+                            fontFamily: 'Oxygen',
+                            fontWeight: '300',
+                            textAlign: 'center',
+                        }}
+                    >
+                        Techs I am aware of.
+                    </Typography>
+                </Grid>
+                <Grid item xs="12">
+                    <Stack direction="row" spacing={2} alignItems="center" justifyContent="center">
+                        {
+                            getTechs().map((el, i, arr) => {
+                                return (
+                                    <Tooltip title={el.techName} arrow>
+                                        <Avatar alt={el.techName} src={el.icon} className="border-2"/>
+                                    </Tooltip>
+                                )
+                            })
+                        }
+                    </Stack>
+                </Grid>
+            </Grid>
+        </>
+    )
 }
 export default Experience
-
--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -13,8 +13,8 @@ const ColorButton = styled(Button)(({ theme }) => ({
   }));
 function Home() {
     return (
-        <div >
-            <Grid container spacing={2} className="bg-dark-blue text-pure-white">
+        <>
+            <Grid container spacing={2} className="bg-dark-blue text-pure-white -mb-19">
                 <Grid item xs={5}>
                     <p className='font-light text-7xl m-4 p-4'>Subhankar Roy.</p>
                     <p className='font-light mx-6 p-2 capitalize'>A 6+ Years experienced UI/UX pro. Open to learn, never settle, &#10084;&#65039; R&D. Worked with clients cross the country.Agile Expert.Certified React JS developer. Spot lighted and awarded Many times for leadership skills. Love to drive, passionate for badminton. </p>
@@ -29,7 +29,7 @@ function Home() {
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#314955" fill-opacity="1" d="M0,64L48,96C96,128,192,192,288,197.3C384,203,480,149,576,112C672,75,768,53,864,80C960,107,1056,181,1152,224C1248,267,1344,277,1392,282.7L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
                 </Grid>
             </Grid>
-        </div>
+        </>
     )
 }
 
