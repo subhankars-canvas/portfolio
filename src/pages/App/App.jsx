2983d1521c94bb95da57a6cbf3527a2a38ec6024
painter on canvas <roysubho687@gmail.com>
2022-06-23 04:14:05 -0700

    added works section and injected tailwind in MUI

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import Header from '../../sections/Header'
 import Home from '../../sections/Home'
 import Experience from '../../sections/Experience'
+import Works from '../../sections/Works'
+import { StyledEngineProvider } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
 function App() {
     return (
-        <>
+        <StyledEngineProvider injectFirst>
+            <CssBaseline />
             <Header/>
             <Home/>
             <Experience/>
-        </>
+            <Works/>
+        </StyledEngineProvider>
     )
 }
 export default App
